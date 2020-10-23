bf9f4628e80cd8a39b3275200279a296e2cafc4f
glance703 <46700484+glance703@users.noreply.github.com>
2020-10-23 14:18:35 +0900

    コレクションフレームワーク課題再提出
     Study.java

--- JavaStudy/2-5/src/study/Study.java
+++ JavaStudy/2-5/src/study/Study.java
@@ -21,13 +21,12 @@ public class Study {
 
     public static void main(String args[]) {
 
-        // ① 定数を全て使って、String型のListを記述てください。
+    	// ① 定数を全て使って、String型のListを記述てください。
     	ArrayList<String> shohinList = new ArrayList<String>();
     	shohinList.add(SHOP_SHOHIN_00);
     	shohinList.add(SHOP_SHOHIN_01);
     	shohinList.add(SHOP_SHOHIN_02);
     	shohinList.add(SHOP_SHOHIN_03);
-
         // ② 以下の「shopMap.put(shohinList.get(1), 180);」の処理について、コメントを記述してください。
         /*
          * [ここへ記述]
@@ -39,7 +38,6 @@ public class Study {
         shopMap.put(shohinList.get(1), 180);
         shopMap.put(shohinList.get(2), 350);
         shopMap.put(shohinList.get(3), 100);
-
         // ③ カリキュラムを参考に拡張for文を使って、課題の画像と同じ表示になるよう記述してください。
         // 「shohinList」と「shopMap」が保持する値を上手く利用しましょう。
         for (Entry<String, Integer> entry : shopMap.entrySet()) {
